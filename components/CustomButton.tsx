ea1df2da49ddf430e6b87ca6ff12b94531d9e660
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-13 07:21:32 +0700

    landing page

---
+++ components/CustomButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Image from "next/image"
+
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({ title, containerStyle, handleClick, btnType } : CustomButtonProps) => {
+  return (
+    <button disabled={false} type={btnType} className={`custom-btn ${containerStyle}`} onClick={handleClick}>
+      <span className={`flex-1`}>
+        {title}
+      </span>
+    </button>
+  )
+}
+
+export default CustomButton
