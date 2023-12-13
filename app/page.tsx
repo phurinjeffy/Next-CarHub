52d98783d30a24b46b82fa44bd78b93211822b2a
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-13 16:15:53 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,55 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
-import { Hero } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2023,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
-    <main className='overflow-hidden'>
+    <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filter">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
-  )
+  );
 }
- 
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,12 +3,22 @@ import Image from "next/image"
 
 import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({ title, containerStyle, handleClick, btnType } : CustomButtonProps) => {
+const CustomButton = ({ title, containerStyle, handleClick, btnType, textStyles, rightIcon, isDisabled } : CustomButtonProps) => {
   return (
     <button disabled={false} type={btnType} className={`custom-btn ${containerStyle}`} onClick={handleClick}>
-      <span className={`flex-1`}>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image 
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   )
 }
